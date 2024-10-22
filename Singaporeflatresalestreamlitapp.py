6f67b585244ced5107648fd29b7a69a74e6383f3
rajimba <159278492+rajimba@users.noreply.github.com>
2024-10-22 19:14:49 +0530

    Singaporeflatresalestreamlitapp.py

--- Singaporeflatresalestreamlitapp.py
+++ Singaporeflatresalestreamlitapp.py
@@ -16,8 +16,8 @@ with st.sidebar:
 if select =="HomePage":
  st.title("Singapore  Resale Flat Prices Predicting Model")
  st.subheader("Singapore  Resale Flat Prices Predicting Model using Pythonscripting, Data Preprocessing, EDA, and Streamlit ")
- image_path = "C:/Users/rajij/Downloads/singapore image.jpg" 
- st.image(image_path, use_column_width=1000)
+
+
 
 elif select=='Modelling':
 
@@ -119,26 +119,26 @@ elif select=='Modelling':
 
             st.table(input_data_)
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/dcr.pkl', 'rb') as file:
+            with open('dcr.pkl', 'rb') as file:
                dcr = pickle.load(file)
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/OE.pkl', 'rb') as file2:
+            with open('OE.pkl', 'rb') as file2:
                loaded_Encoder = pickle.load(file2)
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/OE1.pkl', 'rb') as file2:
+            with open('OE1.pkl', 'rb') as file2:
                loaded_Encoder1 = pickle.load(file2)
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/OE2.pkl', 'rb') as file2:
+            with open('OE2.pkl', 'rb') as file2:
                loaded_Encoder2 = pickle.load(file2)
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/OE3.pkl', 'rb') as file2:
+            with open('OE3.pkl', 'rb') as file2:
                loaded_Encoder3 = pickle.load(file2)
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/OE4.pkl', 'rb') as file2:
+            with open('OE4.pkl', 'rb') as file2:
                loaded_Encoder4 = pickle.load(file2)
 
 
-            with open('C:/Users/rajij/Streamlit_Home Page/Singapore flat resale model/scaler.pkl', 'rb') as file3:
+            with open('scaler.pkl', 'rb') as file3:
                loaded_scaler = pickle.load(file3)
 
 
